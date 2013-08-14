76d366c661b690eb5e52e22751470631bb5376a6
Wush Wu <wush978@gmail.com>
2013-08-14 23:16:47 +0800

    evaluate efficiency

--- tests/WQC2001.R
+++ tests/WQC2001.R
@@ -17,17 +17,21 @@ t <- sapply(y, function(y) {
 	# 	return(as.numeric(ceiling(retval)))
 })
 obj <- new("recurrent-data", model.matrix(~.,iris), y, t, data.frame(), T_0)
-v <- a.var.hat(obj)
 x.eval <- seq(0, obj@T_0, length=100)
 y.eval <- obj$Lambda.hat(x.eval)
-v.eval <- sapply(x.eval, function(x) v(x))
+system.time({
+	v <- a.var.hat(obj)
+	v.eval <- sapply(x.eval, function(x) v(x))
+})
 u.eval <- y.eval + 2*sqrt(v.eval)
 l.eval <- y.eval - 2*sqrt(v.eval)
 plot(x.eval, y.eval, type="l", xlab="x", ylab="Lambda.hat", ylim=c(min(l.eval), max(u.eval)))
 lines(x.eval, l.eval, lty=2)
 lines(x.eval, u.eval, lty=2)
 Lambda.hat <- obj$Lambda.hat
-b.eval <- Lambda.hat(x.eval, bootstrap=TRUE)
+system.time({
+	b.eval <- Lambda.hat(x.eval, bootstrap=TRUE)
+})
 u.eval <- y.eval + 2 * b.eval$error.measurement
 l.eval <- y.eval - 2 * b.eval$error.measurement
 lines(x.eval, u.eval, col=2, lty=2)
