da0f30fc533f4aa95c2adab4eb0462b7277ab9f7
Wush Wu <wush978@gmail.com>
2013-08-19 18:47:35 +0800

    generate:
    1. bootstrap C.I.
    2. asymptotic C.I.
    3. re-generated C.I.

--- tests/WQC2001.R
+++ tests/WQC2001.R
@@ -1,7 +1,8 @@
 library(recurrentR)
-set.seed(1)
+# set.seed(1)
 # lambda <- function(x) exp(sin(x))
 lambda <- function(x) exp(-x/10)
+Lambda <- function(x) 10 * (1 - exp(-x/10))
 T_0 <- rpois(1, 40)
 
 curve(lambda, from=0, to = T_0)
@@ -17,6 +18,14 @@ t <- sapply(y, function(y) {
 	# 	return(as.numeric(ceiling(retval)))
 })
 obj <- new("recurrent-data", model.matrix(~.,iris), y, t, data.frame(), T_0)
+
+c.hat.gen <- recurrentR:::c.hat.gen(obj)
+ci <- sapply(1:length(t), c.hat.gen)
+mean(ci)
+mean(ci^2)
+Lambda(T_0)
+obj$Lambda.hat(T_0)
+
 x.eval <- seq(0, obj@T_0, length=100)
 y.eval <- obj$Lambda.hat(x.eval)
 system.time({
@@ -25,7 +34,9 @@ system.time({
 })
 u.eval <- y.eval + 2*sqrt(v.eval)
 l.eval <- y.eval - 2*sqrt(v.eval)
-plot(x.eval, y.eval, type="l", xlab="x", ylab="Lambda.hat", ylim=c(min(l.eval), max(u.eval)))
+plot(x.eval, Lambda(x.eval), type="l", xlab="x", ylab="Lambda.hat", 
+	col = 3, lwd = 2, ylim=c(min(l.eval), max(u.eval)))
+lines(x.eval, y.eval, lty=1, col = 1)
 lines(x.eval, l.eval, lty=2)
 lines(x.eval, u.eval, lty=2)
 Lambda.hat <- obj$Lambda.hat
@@ -36,5 +47,26 @@ u.eval <- y.eval + 2 * b.eval$error.measurement
 l.eval <- y.eval - 2 * b.eval$error.measurement
 lines(x.eval, u.eval, col=2, lty=2)
 lines(x.eval, l.eval, col=2, lty=2)
-legend("bottomright", c("Lambda.hat", "asymptotic pointwise C.I.", "bootstrap pointwise C.I."), lty=c(1, 2, 2), col=c(1, 1, 2))
+legend("bottomright", c("Lambda.hat", "Lambda", "asymptotic pointwise C.I.", "bootstrap pointwise C.I."), lty=c(1, 1, 2, 2), col=c(1, 3, 1, 2), lwd=c(1, 2, 1, 1))
+
+m <- 100
+real.sim <- matrix(0, length(x.eval), m)
+pb <- txtProgressBar(style=3, max=m)
+for(i in 1:m) {
+	t.sim <- sapply(y, function(y) {
+		# 	browser()
+		lambda_i <- function(t) exp(rnorm(1)) * lambda(t)
+		retval <- gen_inhomo_poisson(lambda_i, y - 1, lambda_i(0))
+		if (is.null(retval)) return(vector("numeric", 0))
+		return(retval)
+		# 	return(as.numeric(ceiling(retval)))
+	})
+	obj.sim <- new("recurrent-data", model.matrix(~.,iris), y, t.sim, data.frame(), T_0)
+	real.sim[,i] <- obj.sim$Lambda.hat(x.eval)
+	setTxtProgressBar(pb, i)
+}
+close(pb)
+
+lines(x.eval, apply(real.sim, 1, function(a) quantile(a, 0.025)), lty=2, col=3, lwd=2)
+lines(x.eval, apply(real.sim, 1, function(a) quantile(a, 0.975)), lty=2, col=3, lwd=2)
 
