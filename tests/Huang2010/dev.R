4b33d5bb74e2b1232846138417b7ca1416115bb2
Wush Wu <wush978@gmail.com>
2013-11-06 16:59:27 +0800

    add tests

--- tests/Huang2010/dev.R
+++ tests/Huang2010/dev.R
@@ -3,7 +3,10 @@ data(huang_2010)
 obj.raw <- attr(huang_2010, "restore")(huang_2010[[2]]) 
 obj <- create_recurrent_data(
   matrix(obj.raw$W, ncol=1), y=obj.raw$Y, t=obj.raw$t, 
-  W=lapply(obj.raw$X, function(x) function(t) c(x * log(t), x * sin(t))), 10, obj.raw$Y < 10
+  W=lapply(obj.raw$X, function(x) {
+    force(x)
+    function(t) c(x * log(t), x * sin(t))
+  }), 10, obj.raw$Y < 10
 )
 
 rho.gen <- function(obj, i, k) {
@@ -13,18 +16,41 @@ rho.gen <- function(obj, i, k) {
 }
 
 h <- function(obj, i, j, beta) {
-  browser()
+#   browser()
   upper_bound <- min(obj@y[i], obj@y[j])
   index_i <- which(obj@t[[i]] < upper_bound)
   index_j <- which(obj@t[[j]] < upper_bound)
-  rho <- Vectorize(rho.gen(obj, i, j))
-  retval <- 1
+  rho <- rho.gen(obj, i, j)
+  W.dim <- length(obj@W[[1]](0))
+  retval <- rep(0, W.dim)
   for(i1 in index_i) {
     for(i2 in index_j) {
-      retval <- retval * 1/(1 + exp(as.vector(rho(obj@t[[i]][i1], obj@t[[j]][i2])) %*% beta))
+      rho.value <- as.vector(rho(obj@t[[i]][i1], obj@t[[j]][i2]))
+      rho.value.beta <- as.vector(rho.value %*% beta)
+      retval <- retval - (exp(rho.value.beta) / (1 + exp(rho.value.beta))) * rho.value
     }
   }
   retval
 }
 
-h(obj, 1, 2, 0.5)
+h(obj, 1, 2, c(0.5, 0.3))
+
+S <- function(obj, beta) {
+  n <- length(obj@y)
+  stopifnot(n > 1)
+  W.dim <- length(obj@W[[1]](0))
+  retval <- rep(0, W.dim)
+  for(i in 1:(n-1)) {
+    for(j in (i+1):n) {
+      retval <- retval + h(obj, i, j, beta)
+    }
+  }
+  retval
+}
+
+S(obj, c(0,0))
+library(nleqslv)
+f <- function(x) S(obj, x)
+library(microbenchmark)
+microbenchmark(f(c(0,0)))
+r <- nleqslv(c(0,0), f)
