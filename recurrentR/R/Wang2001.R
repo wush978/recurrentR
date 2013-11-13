f54f8023d46a4f86dd62656b36d16f2a1f0e8ca7
Wush Wu <wush978@gmail.com>
2013-11-14 01:01:27 +0800

    shift the code write before and prepare to re-implement b_i.hat

--- recurrentR/R/Wang2001.R
+++ recurrentR/R/Wang2001.R
@@ -55,3 +55,86 @@ gamma.hat.gen <- function(obj) {
   }
   obj@cache[["gamma.hat"]]
 }
+
+Q.hat <- function(obj) {
+  s <- obj@s
+  y <- sapply(c(0, s), function(u) mean(sapply(obj@t, function(t) sum(t <= u))))
+  f <- stepfun(s, y)
+  return(f)
+}
+
+Q.hat.c <- function(obj) {
+  s <- obj@s
+  d <- obj@d
+  N <- cumsum(d)
+  new(StepFunction, s, c(0, N)/length(obj@y))
+}
+
+R.hat <- function(obj) {
+  s <- obj@s
+  y <- sapply(c(0, s), function(u) mean(sapply(1:length(obj@t), function(i) {
+    sum(obj@t[[i]] <= u & u <= obj@y[i])
+  })))
+  f <- stepfun(s, y)
+  return(f)
+}
+
+R.hat.c <- function(obj) {
+  s <- obj@s
+  d <- obj@d
+  N <- cumsum(d)
+  y.i <- order(obj@y)
+  m <- sapply(obj@t, length)
+  N <- N + eval_N(s, obj@y[y.i], m[y.i])
+  new(StepFunction, s, c(0, N/length(obj@y)))	
+}
+
+b.hat <- function(obj, i) {
+  R <- R.hat(obj)
+  R.t <- R(obj@t[[i]])
+  k.single <- function(u) {
+    if (u > obj@y[i]) return(0)
+    sum(obj@t[[i]] <= u) / R(u)^2		
+  }
+  k <- function(u) {
+    sapply(u, k.single)
+  }
+  function(t) {
+    step_integrate(k, Q.hat(obj), t, obj@T_0) - sum(as.numeric(t < obj@t[[i]]) / R.t)
+  }
+}
+
+b.hat.gen <- function(obj) {
+  R <- R.hat.c(obj)
+  Q <- Q.hat.c(obj)
+  return(function(i) {
+    R.t <- R$sort_call(obj@t[[i]])
+    x <- c(obj@t[[i]], obj@y[i])
+    y <- append(0:length(obj@t[[i]]), 0)
+    k.numerator <- new(StepFunction, x, y)
+    k <- k.numerator / R^2
+    function(t) {
+      if (t == obj@T_0) return(0)
+      step_integrate.StepFunction(k, Q, t, obj@T_0) - sum(as.numeric(t < obj@t[[i]]) / R.t)
+    }
+  })
+}
+
+b.hat.y <- function(obj) {
+  R <- R.hat.c(obj)
+  Q <- Q.hat.c(obj)
+  retval <- matrix(0.0, obj@n, obj@n)
+  for(i in seq_len(obj@n)) {
+    R.t <- R$sort_call(obj@t[[i]])
+    x <- c(obj@t[[i]], obj@y[i])
+    y <- append(0:length(obj@t[[i]]), 0)
+    k.numerator <- new(StepFunction, x, y)
+    k <- k.numerator / R^2
+    for(j in seq_len(obj@n)) {
+      t <- obj@y[j]
+      if (t == obj@T_0) next
+      retval[i,j] <- step_integrate.StepFunction(k, Q, t, obj@T_0) - sum(as.numeric(t < obj@t[[i]]) / R.t)
+    }
+  }
+  retval
+}
--- recurrentR/src/Wang2001.cpp
+++ recurrentR/src/Wang2001.cpp
@@ -89,3 +89,34 @@ SEXP eval_N(NumericVector s, NumericVector y, IntegerVector m) {
 	return retval;
 	END_RCPP
 }
+
+template<typename T>
+bool leq(T a, T b) {
+  return a <= b;
+}
+
+inline int bupper(NumericVector x, double a) {
+	return std::upper_bound(x.begin(), x.end(), a, leq<double>) - x.begin() + 1;
+}
+
+inline int blower(NumericVector x, double b) {
+	return std::lower_bound(x.begin(), x.end(), b) - x.begin();
+}
+
+RcppExport SEXP substring_index(SEXP Rx, SEXP Ra, SEXP Rb) {
+  BEGIN_RCPP
+	NumericVector x(Rx);
+	double a(as<double>(Ra));
+	double b(as<double>(Rb));
+	int begin_index = bupper(x, a);
+	if (begin_index > x.size()) return R_NilValue;
+	int end_index = blower(x, b);
+	if (end_index == 0) return R_NilValue;
+	if (begin_index > end_index) return R_NilValue;
+	IntegerVector retval(end_index - begin_index + 1, 0);
+	for(int i = begin_index;i <= end_index;i++) {
+		retval[i - begin_index] = i;
+	}
+	return retval;
+	END_RCPP
+}
