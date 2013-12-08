54a6d198b9802fba196cc1eca3269f394c8f7fb9
Wush Wu <wush978@gmail.com>
2013-12-08 15:00:06 +0800

    Extend StepFunction for lea

--- recurrentR/src/Wang2001.cpp
+++ recurrentR/src/Wang2001.cpp
@@ -7,25 +7,38 @@ typedef std::vector<double> NumVec;
 struct StepFunction {
   NumVec x;
 	NumVec y;
+  bool lower_bound;
 	
 	StepFunction(NumericVector Rx, NumericVector Ry) 
-	: x(as<NumVec>(Rx)), y(as<NumVec>(Ry)) { 
+	: x(as<NumVec>(Rx)), y(as<NumVec>(Ry)), lower_bound(false) { 
 		if (x.size() + 1 != y.size()) throw std::invalid_argument("length(Rx) + 1 == length(Ry) does not hold");
 	}
 	~StepFunction() { }
 	
 	double call(double input) const {
-		NumVec::const_iterator i = std::upper_bound(x.begin(), x.end(), input);
+    NumVec::const_iterator i;
+		if (lower_bound) {
+      i = std::lower_bound(x.begin(), x.end(), input);
+		} else {
+      i = std::upper_bound(x.begin(), x.end(), input);
+		}
 		return y[i - x.begin()];
 	}
 	
 	SEXP sort_call(NumericVector input) {
 		NumVec::const_iterator i = x.begin();
 		NumericVector retval(input.size());
-		for(int j = 0;j < input.size();j++) {
-			while(*i <= input[j] & i != x.end()) { i++; }
-			retval[j] = y[i - x.begin()];
-		}
+    if (lower_bound) {
+    	for(int j = 0;j < input.size();j++) {
+  			while(*i < input[j] & i != x.end()) { i++; }
+  			retval[j] = y[i - x.begin()];
+  		}
+    } else {
+  		for(int j = 0;j < input.size();j++) {
+  			while(*i <= input[j] & i != x.end()) { i++; }
+  			retval[j] = y[i - x.begin()];
+  		}
+    }
 		return retval;
 	}
 	
@@ -42,6 +55,7 @@ RCPP_MODULE(StepFunction) {
 	.constructor<NumericVector, NumericVector>()
 	.field_readonly("x", &StepFunction::x)
 	.field_readonly("y", &StepFunction::y)
+  .field("lower_bound", &StepFunction::lower_bound)
 	.method("call", &StepFunction::call)
 	.method("sort_call", &StepFunction::sort_call)
 	;
